b614ade3d77814fc4a4995a05bc51d7abea511e4
Eran Zaksh <eranzaksh@gmail.com>
2023-06-08 11:13:44 +0300

    Starting to work on csv

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from movie_app import MovieApp
 from storage_json import StorageJson
+from storage_csv import StorageCsv
 
-storage = StorageJson('data.json')
+#storage = StorageJson('data.json')
+storage = StorageCsv(movies.csv)
 movie_app = MovieApp(storage)
 movie_app.run()
--- storage_csv.py
+++ storage_csv.py
@@ -1,7 +1,9 @@
+import csv
+
 from istorage import IStorage
 
 
-class Storage_csv(IStorage):
+class StorageCsv(IStorage):
     def __init__(self, filepath):
         self.filepath = filepath
 
@@ -11,5 +13,29 @@ class Storage_csv(IStorage):
         Make a print statement in red color
         """
         print("\033[91m {}\033[00m".format(skk))
+
+    def add_movie(self, title, year, rating, poster, imdb_page):
+        pass
+
+    def list_movies(self):
+        with open(self.filepath, 'r') as csvfile:
+            csvreader = csv.DictReader(csvfile)
+            # Convert CSV data to a list of dictionaries
+            data = [dict(row) for row in csvreader]
+        result = {}
+        print(data)
+        for row in data:
+            title = row['Title']
+            rating = int(float(row['Rating']))  # Convert rating to integer
+            result[title] = {
+                'Rating': rating,
+                'Year': row['Year']
+            }
+
+    def update_movie(self, title, notes):
+        pass
+
+    def delete_movie(self, title):
+        pass
 
 
